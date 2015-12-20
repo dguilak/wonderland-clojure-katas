3958f8790468fe57219bf39c85fc2b515a73c115
Daniel Guilak <daniel.guilak@gmail.com>
2015-12-19 18:50:02 -0600

    Day 1 - Alphabet Cipher, encode

--- alphabet-cipher/src/alphabet_cipher/coder.clj
+++ alphabet-cipher/src/alphabet_cipher/coder.clj
@@ -1,7 +1,16 @@
 (ns alphabet-cipher.coder)
 
+(defn alpha-range [begin end]
+  "Gives all characters between `begin' and `end'"
+  (map char (range (int begin) (inc (int end)))))
+
+(defn substitution-chart [x-char y-char]
+  "Gives the substitution chart for a particular letter-letter combination"
+  (nth (cycle (alpha-range \a \z)) (+ (- (int y-char) 97) (- (int x-char) 97))))
+
 (defn encode [keyword message]
-  "encodeme")
+  "Encodes `keyword' with `message' using the alphabet cipher"
+  (apply str (map substitution-chart (cycle (seq keyword)) message)))
 
 (defn decode [keyword message]
   "decodeme")
