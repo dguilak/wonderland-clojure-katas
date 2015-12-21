a85fb685efd98c50376133ab95c19b86751a9104
Daniel Guilak <daniel.guilak@gmail.com>
2015-12-21 09:29:29 -0800

    Needs cleanup: Day 2 decode

--- alphabet-cipher/src/alphabet_cipher/coder.clj
+++ alphabet-cipher/src/alphabet_cipher/coder.clj
@@ -1,5 +1,8 @@
 (ns alphabet-cipher.coder)
 
+(defn abs [n]
+  (max n (- n)))
+
 (defn alpha-range [begin end]
   "Gives all characters between `begin' and `end'"
   (map char (range (int begin) (inc (int end)))))
@@ -8,13 +11,15 @@
   "Gives the substitution chart for a particular letter-letter combination"
   (nth (cycle (alpha-range \a \z)) (+ (- (int y-char) 97) (- (int x-char) 97))))
 
+(defn lookup-by-value-and-key [key encoded]
+  (char (+ 97 (count (take-while #(not= % encoded) (drop-while #(not= % key) (cycle (alpha-range \a \z))))))))
+
 (defn encode [keyword message]
   "Encodes `keyword' with `message' using the alphabet cipher"
   (apply str (map substitution-chart (cycle (seq keyword)) message)))
 
 (defn decode [keyword message]
-  "decodeme")
+  (apply str (map lookup-by-value-and-key (cycle (seq keyword)) message)))
 
 (defn decipher [cipher message]
   "decypherme")
-
