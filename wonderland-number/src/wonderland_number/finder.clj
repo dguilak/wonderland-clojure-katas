c154c6bd13e48145ca491cc04d3a2484da12bb57
Daniel Guilak <daniel.guilak@gmail.com>
2015-12-21 20:11:17 -0800

    cleanup wonderland magic number

--- wonderland-number/src/wonderland_number/finder.clj
+++ wonderland-number/src/wonderland_number/finder.clj
@@ -1,12 +1,7 @@
 (ns wonderland-number.finder)
 
 (defn is-magic-number? [n]
-  (let [n2 (-> (* n 2) str set)
-        n3 (-> (* n 3) str set)
-        n4 (-> (* n 4) str set)
-        n5 (-> (* n 5) str set)
-        n6 (-> (* n 6) str set)]
-    (= n2 n3 n4 n5 n6)))
+  (apply = (map (comp set str (partial * n)) (range 2 7))))
 
 (defn wonderland-number []
-  (first (filter is-magic-number? (range 100000 999999))))
+  (first (filter is-magic-number? (range 100000 999999)))
