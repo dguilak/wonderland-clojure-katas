349a2d109706ba9b0941cf756d5a6484297f9101
Daniel Guilak <daniel.guilak@gmail.com>
2015-12-21 17:53:39 -0800

    made alphabet cipher cleaner

--- alphabet-cipher/src/alphabet_cipher/coder.clj
+++ alphabet-cipher/src/alphabet_cipher/coder.clj
@@ -3,16 +3,22 @@
 (defn abs [n]
   (max n (- n)))
 
+(def ascii-offset (int \a))
+
 (defn alpha-range [begin end]
   "Gives all characters between `begin' and `end'"
   (map char (range (int begin) (inc (int end)))))
 
 (defn substitution-chart [x-char y-char]
   "Gives the substitution chart for a particular letter-letter combination"
-  (nth (cycle (alpha-range \a \z)) (+ (- (int y-char) 97) (- (int x-char) 97))))
+  (nth (cycle (alpha-range \a \z)) (+ (- (int y-char) ascii-offset) (- (int x-char) ascii-offset))))
 
 (defn lookup-by-value-and-key [key encoded]
-  (char (+ 97 (count (take-while #(not= % encoded) (drop-while #(not= % key) (cycle (alpha-range \a \z))))))))
+  (->> (drop-while #(not= % key) (cycle (alpha-range \a \z)))
+       (take-while #(not= % encoded))
+       count
+       (+ ascii-offset)
+       char))
 
 (defn encode [keyword message]
   "Encodes `keyword' with `message' using the alphabet cipher"
