87374bd54c9ad2e7f7cd4b4601a1aa5a38426c2d
Daniel Guilak <daniel.guilak@gmail.com>
2015-12-21 17:54:12 -0800

    week 4 day 3 - wonderland number kata, unclean

--- wonderland-number/src/wonderland_number/finder.clj
+++ wonderland-number/src/wonderland_number/finder.clj
@@ -1,5 +1,12 @@
 (ns wonderland-number.finder)
 
+(defn is-magic-number? [n]
+  (let [n2 (-> (* n 2) str set)
+        n3 (-> (* n 3) str set)
+        n4 (-> (* n 4) str set)
+        n5 (-> (* n 5) str set)
+        n6 (-> (* n 6) str set)]
+    (= n2 n3 n4 n5 n6)))
+
 (defn wonderland-number []
-  ;; calculate me
-  42)
+  (first (filter is-magic-number? (range 100000 999999))))
